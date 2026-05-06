248b4829d67c68e33a8a700a87632d0440c5cb3f
Junyan Xu <junyanxu.math@gmail.com>
2026-05-07 00:23:15 +0200

    add some more definitions

--- LeanEval/Topology/Prelude.lean
+++ LeanEval/Topology/Prelude.lean
@@ -1,3 +1,6 @@
+import Mathlib.Algebra.Category.Grp.Basic
+import Mathlib.Algebra.Category.Grp.Abelian
+import Mathlib.AlgebraicTopology.SingularHomology.Basic
 import Mathlib.Analysis.Complex.Circle
 import Mathlib.Analysis.InnerProductSpace.PiL2
 import EvalTools.Markers
@@ -5,15 +8,72 @@ import EvalTools.Markers
 /-!
 # Basic definitions in topology
 
-In this file we define a representative surface in each homeomorphism class of surfaces by
-gluing certain arcs in the boundary of the unit disc.
+## Main definitions
 
-We also define a representative 3-dimensional handlebody for each genus.
++ `singularHomology`, `bettiNumber`: the singular homology and Betti numbers of a topological
+space with coefficients in an abelian group.
+
++ `ContinuousMap.toEndSingularHomology`: the homomorphism from the monoid of continuous maps
+from a space to itself to the endomorphism monoid of one of its singular homology groups.
+
++ `Surface.OrientableRepr`, `Surface.NonOrientableRepr`: a representative surface in each
+homeomorphism class of surfaces, obtained by gluing certain arcs in the boundary of the unit disc.
+
++ `handlebody₃`: a representative 3-dimensional handlebody for each genus.
 See https://en.wikipedia.org/wiki/Handlebody#3-dimensional_handlebodies.
 
 We pose it as a LeanEval problem to show that the boundary of the handlebody is homeomorphic
 to the corresponding representative surface.
 -/
+
+section Topology
+
+variable (X : Type*) [TopologicalSpace X]
+
+instance : Monoid C(X, X) where
+  mul := .comp
+  mul_assoc _ _ _ := rfl
+  one := .id X
+  one_mul _ := rfl
+  mul_one _ := rfl
+
+instance : Group (X ≃ₜ X) where
+  mul f g := g.trans f
+  mul_assoc _ _ _ := rfl
+  one := .refl _
+  one_mul _ := rfl
+  mul_one _ := rfl
+  inv := .symm
+  inv_mul_cancel f := by ext; apply f.left_inv
+
+def Homeomorph.toContinuousMap : (X ≃ₜ X) →* C(X, X) where
+  toFun := (↑)
+  map_one' := rfl
+  map_mul' _ _ := rfl
+
+end Topology
+
+section AlgebraicTopology
+
+open AlgebraicTopology CategoryTheory
+
+universe u in
+instance : Limits.HasCoproducts.{u} AddCommGrpCat.{u} := inferInstance
+
+variable (n : ℕ) (X : Type*) [TopologicalSpace X] (G : Type) [AddCommGroup G]
+
+noncomputable abbrev singularHomology :=
+  ((singularHomologyFunctor AddCommGrpCat n).obj (.of (ULift G))).obj (.of X)
+
+noncomputable def bettiNumber : ℕ := Module.finrank ℤ <| singularHomology n X ℤ
+
+noncomputable def ContinuousMap.toEndSingularHomology :
+    C(X, X) →* AddMonoid.End (singularHomology n X G) where
+  toFun f := (((singularHomologyFunctor AddCommGrpCat n).obj _).map <| TopCat.ofHom f).hom
+  map_one' := AddCommGrpCat.ofHom_injective <| by simp; exact CategoryTheory.Functor.map_id ..
+  map_mul' _ _ := AddCommGrpCat.ofHom_injective <| by simp; exact Functor.map_comp ..
+
+end AlgebraicTopology
 
 namespace Complex
 
@@ -66,22 +126,22 @@ end Surface
 open Set
 
 /-- A 3-dimensional handlebody of genus `g`. -/
-def handlebody (g : ℕ) : Set (ℝ × ℝ × ℝ) :=
+def handlebody₃ (g : ℕ) : Set (ℝ × ℝ × ℝ) :=
   Icc 0 1 ×ˢ (Icc 0 (2 * (g : ℝ) + 1) ×ˢ Icc 0 3 \ ⋃ i : Fin g, Ioo (2 * (i : ℝ) + 1) (2 * i + 2) ×ˢ Ioo 1 2)
 
 @[eval_problem]
 def frontierHandlebodyHomeomorphOrientableRepr (g : ℕ) (hg : g ≠ 0) :
-    frontier (handlebody g) ≃ₜ Surface.OrientableRepr g 0 := by
+    frontier (handlebody₃ g) ≃ₜ Surface.OrientableRepr g 0 := by
   sorry
 
 @[eval_problem]
 def frontierHandlebodyHomeomorphSphere :
-    frontier (handlebody 0) ≃ₜ Metric.sphere (0 : EuclideanSpace ℝ (Fin 3)) 1 := by
+    frontier (handlebody₃ 0) ≃ₜ Metric.sphere (0 : EuclideanSpace ℝ (Fin 3)) 1 := by
   sorry
 
 @[eval_problem]
 def frontierHandlebodyHomeomorphAddCircleProd :
-    frontier (handlebody 1) ≃ₜ AddCircle (1 : ℝ) × AddCircle (1 : ℝ) := by
+    frontier (handlebody₃ 1) ≃ₜ AddCircle (1 : ℝ) × AddCircle (1 : ℝ) := by
   sorry
 
 end LeanEval.Topology
