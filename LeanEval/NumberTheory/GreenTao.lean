3bb7ce03ebee64faf73fb1db7a25c2bebe53bde4
twwar <tom.waring@unimelb.edu.au>
2026-05-15 10:16:36 +0200

    better statement

--- LeanEval/NumberTheory/GreenTao.lean
+++ LeanEval/NumberTheory/GreenTao.lean
@@ -8,11 +8,7 @@ There exist arbitrarily-long arithmetic progression of primes.
 Ben Green and Terence Tao. *The primes contain arbitrarily long arithmetic progressions* (2004).
 -/
 
-/-- (Possibly trivial) arithmetic progression in any type equipped with addition. -/
-def List.IsArithProg {α : Type*} [Add α] (l : List α) : Prop :=
-  ∃ d, ∀ x y, [x, y] <:+: l → x + d = y
-
 /-- The *Green-Tao Theorem*: there are arbitrarily long arithmetic progressions of primes. -/
 @[eval_problem]
 theorem green_tao (k : ℕ) :
-  ∃ ps : List ℕ, (∀ p ∈ ps, p.Prime) ∧ ps.IsArithProg ∧ k ≤ ps.length ∧ ps.Nodup := sorry
+  ∃ p, ∃ d > 0, ∀ i < k, (p + i * d).Prime := sorry
