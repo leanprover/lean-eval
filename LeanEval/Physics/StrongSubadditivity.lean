61e0449860990d199ed1c6e7e73e75a2f3818fc7
Alex Meiburg <timeroot.alex@gmail.com>
2026-05-29 12:47:40 -0400

    Update StrongSubadditivity.lean

--- LeanEval/Physics/StrongSubadditivity.lean
+++ LeanEval/Physics/StrongSubadditivity.lean
@@ -11,6 +11,9 @@ the significance of this inequality.
 
 -/
 
+namespace LeanEval
+namespace Physics
+
 variable {A B C : Type*}
 variable [Fintype A] [Fintype B] [Fintype C]
 variable [DecidableEq A] [DecidableEq B] [DecidableEq C]
@@ -36,10 +39,13 @@ open ComplexOrder
  density matrix to the simpler statement that it's PSD, which holds since normalization just produces
  a positive affine transformation on the entropy. -/
 @[eval_problem]
-theorem StrongSubadditivity (M_ABC : Matrix (A × B × C) (A × B × C) ℂ) (h : M_ABC.PosSemidef) :
+theorem strong_subadditivity (M_ABC : Matrix (A × B × C) (A × B × C) ℂ) (h : M_ABC.PosSemidef) :
     let M_AB : Matrix (A × B) (A × B) ℂ :=
       .traceRight <| M_ABC.reindex (.symm <| .prodAssoc ..) (.symm <| .prodAssoc ..)
     let M_BC : Matrix (B × C) (B × C) ℂ := M_ABC.traceLeft
     let M_B : Matrix B B ℂ := M_BC.traceRight
     entropy M_ABC + entropy M_B ≤ entropy M_AB + entropy M_BC := by
   sorry
+
+end Physics
+end LeanEval
