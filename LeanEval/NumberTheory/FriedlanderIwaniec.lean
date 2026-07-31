5b23116052d8cab53d98413e8eab5329c4bbdf5f
Bolton Bailey <bolton.bailey@gmail.com>
2026-07-31 08:31:58 -0700

    Update LeanEval/NumberTheory/FriedlanderIwaniec.lean
    Co-authored-by: Junyan Xu <junyanxu.math@gmail.com>

--- LeanEval/NumberTheory/FriedlanderIwaniec.lean
+++ LeanEval/NumberTheory/FriedlanderIwaniec.lean
@@ -16,14 +16,8 @@ There are infinitely many primes of the form `a² + b⁴`.
  See <https://en.wikipedia.org/wiki/Friedlander%E2%80%93Iwaniec_theorem>.
 -/
 
-section Main
-
-open Nat
-
 @[eval_problem]
 theorem friedlander_iwaniec : {p : ℕ | p.Prime ∧ ∃ a b, p = a ^ 2 + b ^ 4}.Infinite := by
   sorry
-
-end Main
 
 end LeanEval.NumberTheory.FriedlanderIwaniec
