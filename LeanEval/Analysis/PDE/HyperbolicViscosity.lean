308ba3151e9c4d249e83ca269277e596b871c699
Joris van Winden <j.van.winden99@gmail.com>
2026-07-31 15:53:13 +0200

    tag as eval problem

--- LeanEval/Analysis/PDE/HyperbolicViscosity.lean
+++ LeanEval/Analysis/PDE/HyperbolicViscosity.lean
@@ -59,6 +59,7 @@ structure HasTVEstimateWith (δ C : EReal) : Prop where
       (h_initial_lim : ∃ x ∈ K, Tendsto (u 0 ·) atBot (𝓝 x))
       t (ht : t > 0) : eVariationOn (u t ·) univ ≤ C * eVariationOn (u 0 ·) univ
 
+@[eval_problem]
 theorem hyperbolic_tv_estimate (hn : 1 ≤ n) (hK : IsCompact K) (h : IsHyperbolicOn n A K) :
     ∃ δ > 0, ∃ C < ⊤, HasTVEstimateWith n A K δ C :=
   sorry
