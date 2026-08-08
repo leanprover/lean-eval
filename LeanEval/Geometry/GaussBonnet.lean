02dd28667b7c1edb35a62588f2d205f37f9782cb
Oliver Nash <github@olivernash.org>
2026-08-08 12:42:29 +0100

    Fix incorrect statement.
    I am grateful to Adam Topaz for pointing out this error.
    
    Before this change, although the _goal_ correctly accounted for the unnormalied
    scaling of the Hausdorff measure, the formula `hK` did not. Since `hK` tells us
    that to order `r ^ 2`, the area of a ball is `πr ^ 2`, the statement contained
    a contradiction since with the unnormalied Hausdorff scaling this should be
    `4r ^ 2`.
    
    I could have fixed this bug by scaling the expression `Q` appearing in `hK`
    by a factor of `4 / π` but instead decided to introduce a new measure `μ`
    together with a requirement that it has the conventional scaling. This has the
    advantage that both the formula for `hK` and the goal contain conventional,
    familiar constants.

--- LeanEval/Geometry/GaussBonnet.lean
+++ LeanEval/Geometry/GaussBonnet.lean
@@ -6,10 +6,7 @@ import EvalTools.Markers
 
 There are many versions of the Gauss-Bonnet theorem. Here we propose the problem
 to prove the two-dimensional, boundaryless, not-necessarily-oriented version for
-metrics which arise from smooth Riemannian metrics. The usual statement is that if
-`K` is the Gaussian curvature and `χ` is the Euler characteristic then: `∫ K = 2πχ`.
-However since we integrate using Mathlib's Hausdorff measure which is unnormalised
-the formula instead reads `∫ K = 8χ`.
+metrics which arise from smooth Riemannian metrics.
 
 We do not define the Euler characteristic by purely topological methods and instead
 merely require a proof that the integral of the curvature quantises.
@@ -22,14 +19,12 @@ open scoped ContDiff Manifold MeasureTheory Real Topology
 
 local notation "𝔼²" => EuclideanSpace ℝ (Fin 2)
 
-open Bundle Filter Metric in
+open Bundle Filter MeasureTheory Metric in
 /-- Note that since we don't assume oriented, the Euler characteristic need not be even.
 
-Note also that Mathlib's Hausdorff measure is the unnormalised version. E.g., if `E` is a
-finite-dimensional real `d`-dimensional normed space (with borel sigma algebra) then
-`μH[d] (ball (0 : E) 1) = 2 ^ d`. Thus `μH[2]` is scaled from the
-conventional measure by `4 / π`. This means that the constant in Gauss-Bonnet becomes `8`
-instead of `2π`.
+Note also that Mathlib's Hausdorff measure is "unnormalised". To make the statement appear
+with familiar constants, we state the theorem using a measure `μ` which is `4 / π` times
+the Hausdorff area measure.
 
 Note finally that we do not include assumptions `IsContinuousRiemannianBundle` or
 `BoundarylessManifold` since these follow from the other assumptions. -/
@@ -41,12 +36,14 @@ theorem gauss_bonnet
     [RiemannianBundle (B := M) (TangentSpace (𝓡 2))]
     [IsContMDiffRiemannianBundle (B := M) (𝓡 2) ∞ 𝔼² (TangentSpace (𝓡 2))]
     [IsRiemannianManifold (𝓡 2) M]
+    (μ : Measure M)
+    (hμ : μ = (NNReal.pi / 4) • μH[2])
     (K : M → ℝ)
     (hK : ∀ x,
-      letI A := fun r ↦ (μH[2] (ball x r)).toReal
+      letI A := fun r ↦ (μ (ball x r)).toReal
       letI Q := fun r ↦ 12 * (π * r ^ 2 - A r) / (π * r ^ 4)
       Tendsto Q (𝓝[>] 0) (𝓝 (K x))) :
-    ∃ n : ℕ, ∫ x, K x ∂μH[2] = 8 * (2 - (n : ℝ)) := by
+    ∃ n : ℕ, ∫ x, K x ∂μ = 2 * π * (2 - (n : ℝ)) := by
   sorry
 
 end LeanEval.Geometry
